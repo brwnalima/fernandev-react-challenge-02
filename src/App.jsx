5e23a51f8c4b731e6ff1b04c5535c8f276610b37
brwnalima <bruna.lima@edu.unirio.br>
2023-04-21 14:11:28 -0300

    teste realizado

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { login } from './utils';
 import './index.css';
 import { useState } from 'react';
 
+
 // Instruções:
 // * Você tem um formulário de login INCOMPLETO
 // * Não é permitido adicionar novos elementos HTML
@@ -9,29 +10,58 @@ import { useState } from 'react';
 //
 // Tarefas:
 // done- O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
-// todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
-// todo - Desabilite o botão de Login equanto você está executando o login.
-// todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
-// todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
+// done - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
+// done - Desabilite o botão de Login equanto você está executando o login.
+// done - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
+// done - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
 
 export default function LoginForm() {
+
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
+  const [error, setError] = useState(null);
+  const [isRequesting, setIsRequesting] = useState(false);
+
+  const handleSubmit = () => {
+    console.log("clicou");
+
+    setError(null);
+    setIsRequesting(true);
+
+    let values = { email: input1, password: input2 }
+
+    login(values)
+      //tratando a promise
+      .then((sucess) => {
+        alert(sucess.message)
+      })
+      .catch((error) => {
+        console.log(error.message);
+        setError(error.message)
+      })
+      .finally(() => {
+        setIsRequesting(false)
+      }) // finally roda sempre, independente se cai no then ou catch
+  }
+
   return (
     <div className='wrapper'>
       <div className='login-form'>
         <h1>Login Form 🐞</h1>
         {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
-        <div className='errorMessage'></div>
+        {error && <div className='errorMessage'>{error}</div>}
         <div className='row'>
           <label htmlFor={'email'}>Email</label>
-          <input id={'email'} type={'email'} autoComplete='off' />
+          <input id={'email'} type={'email'} value={input1} onChange={(e) => setInput1(e.target.value)} autoComplete='off' />
         </div>
         <div className='row'>
           <label htmlFor={'password'}>Password</label>
-          <input id={'password'} type={'password'} />
+          <input id={'password'} type={'password'} value={input2} onChange={(e) => setInput2(e.target.value)} />
         </div>
 
         <div className='button'>
-          <button id='login' onClick={login}>Login</button>
+          <button id='login' disabled={input2.length < 6 || input1 === "" || isRequesting}
+            onClick={handleSubmit}>Login</button>
         </div>
       </div>
     </div>
--- src/utils.js
+++ src/utils.js
@@ -1,46 +1,17 @@
-const e = document.getElementById("email")
-const p = document.getElementById("password")
-const btnLogin = document.getElementById("login")
 
-e.addEventListener('input', verificarDados)
-p.addEventListener('input', verificarDados)
-
-function verificarDados() {
-  const emailValue = e.value;
-  const senhaValue = p.value;
-
-  if (senhaValue.length < 6 || emailValue.trim() === "") {
-    btnLogin.setAttribute("disabled", "true")
-  } else {
-    btnLogin.removeAttribute("disabled")
-  }
-}
-
-export function login({ email, password }) {
-  const delay = (0.1 + Math.random() * 2) * 1000;
-
-  // Instruções:
-  // * Você tem um formulário de login INCOMPLETO
-  // * Não é permitido adicionar novos elementos HTML
-  // * Não é permitido usar refs
-  //
-  // Tarefas:
-  // done- O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
-  // todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
-  // todo - Desabilite o botão de Login equanto você está executando o login.
-  // todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
-  // todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
+export function login({ email, password}) 
+/* é um objeto com dois componentes e não um atributo */ {
+  const delay = (0.2  + Math.random() * 2) * 1000;
 
   return new Promise((resolve, reject) => {
-
-
     setTimeout(function () {
       if (password === 'password123' && !!email) {
-        resolve();
+        resolve({ message: 'Login realizado com sucesso!' });
       }
       else {
-        reject({ message: 'e-mail or password wrong.' });
+        reject({ message: 'E-mail ou senha não encontrado.' });
       }
     }, delay);
   })
 }
+
